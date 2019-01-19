4eda01fa5e2339edb3559f31fe2beee025da9902
marko <1995marko@gmail.com>
2019-01-19 11:33:14 +0100

    Minor changes for readability

--- session.py
+++ session.py
@@ -8,13 +8,14 @@ class Track_session:
 
     def __init__(self, eye, pupil_param: list, resolution = 100):
         self.eye = eye
-        gaze_amount = len(pupil_param)
-        self.__gaze_number = range(0,gaze_amount)
-        self.pupil_param = pupil_param
-        self.__pupil_circle= np.empty((gaze_amount, resolution, 3))
+        self.gaze_amount = len(pupil_param)
+        self.__gaze_number = range(0,self.gaze_amount)
+        self.pupils = pupil_param
+        self.pupil_circle= np.empty((self.gaze_amount, resolution, 3))
         for gaze_id in self.__gaze_number:
-            p = projection.Pupil(self.eye, pupil_param[gaze_id])
-            self.__pupil_circle[gaze_id] = p.make_3d_circle(resolution=resolution)
+            p = projection.Pupil(self.eye, self.pupils[gaze_id])
+            self.pupil_circle[gaze_id] = p.make_3d_circle(resolution=resolution)
+
 
 
 
