036cefefb838f9b5674add5e249015e5410453c7
marko <1995marko@gmail.com>
2019-02-12 17:09:56 +0100

    last saving before altering the data structure

--- projection.py
+++ projection.py
@@ -4,34 +4,34 @@ import cv2 as cv
 
 
 class Eye:
-    def __init__(self, center=np.array([0.0,-5.0,0.0]), s_radius=1.2):
+    def __init__(self, center=np.array([0.0,-5.0,0.0]), sphere_radius=1.2):
         '''
         Angaben entprechen cm.
         :param center:
-        :param s_radius:
+        :param sphere_radius:
         '''
-        if center[1] + s_radius > -1:
+        if center[1] + sphere_radius > -1:
             raise ValueError("Eyeball muss weiter nach hinten auf der y-Achse verschoben werden, sonst nicht von Kamera erfasst")
         else:
             self.center   = center
-            self.s_radius = s_radius
-            self.display_distance = round(self.center[1]+self.s_radius)/2 #Should be negative!
+            self.sphere_radius = sphere_radius
+            self.display_distance = round(self.center[1]+self.sphere_radius)/2 #Should be negative!
 
-    def rads_to_cartesians(self,rad_points):
+    def spherical_to_cartesians(self,spherical_points):
         '''
              Neutral pos:: (long, lat, spherical_rad): (0,0,1) -> (0,1,0) in cartesian
              Spherical points are in regards to eye center which is considered for the recalculation
-            :param rad_points: numpy array with shape (points size, 3)
+            :param spherical_points: numpy array with shape (points size, 3)
             :return: cartesian numpy
          '''
-        if rad_points.ndim == 1:
+        if spherical_points.ndim == 1:
             raise TypeError("Nur Matritzen aus mehrere Punkte erlaubt!")
         else:
-            if rad_points.shape[1] == 2:
-                radius = self.s_radius
+            if spherical_points.shape[1] == 2:
+                radius = self.sphere_radius
             else:
-                radius = rad_points[:,2]
-            hor = rad_points[:,0]; vert = rad_points[:,1];
+                radius = spherical_points[:,2]
+            hor = spherical_points[:,0]; vert = spherical_points[:,1];
             x = np.sin(hor) * np.cos(vert) * radius + self.center[0]
             y = np.cos(hor) * np.cos(vert) * radius + self.center[1]
             z = np.sin(vert) * radius + self.center[2]
@@ -72,24 +72,25 @@ class Pupil:
     def __init__(self, eye, spherical_deg, resolution = 100):
         self.long_rad = math.radians(spherical_deg[0])
         self.lat_rad  = math.radians(spherical_deg[1])
-        self.p_radius = spherical_deg[2]
+        self.pupil_radius = spherical_deg[2]
         self.eye = eye
-        self.circle_points = self.make_3d_circle(resolution)
-        self.pupil_center_cart = self.eye.rads_to_cartesians(np.array([[self.long_rad, self.lat_rad]]))
-        self.ellipse_points = self.eye.project_to_2d(self.circle_points)
+        self.circle_points_spherical = self.make_3d_circle(resolution)
+        self.circle_points_cart = self.eye.spherical_to_cartesians(self.circle_points_spherical)
+        self.pupil_center_cart = self.eye.spherical_to_cartesians(np.array([[self.long_rad, self.lat_rad]]))
+        self.ellipse_points = self.eye.project_to_2d(self.circle_points_cart)
         self.ellipse_param = self.get_ellipse_param(self.ellipse_points)
 
     def make_3d_circle(self, resolution):
         '''
         :return: Cartesian 3D coordinates from pupil
         '''
-        self.pupil_radius_rad = np.arcsin(self.p_radius/self.eye.s_radius)
+        self.pupil_radius_rad = np.arcsin(self.pupil_radius/self.eye.sphere_radius)
 
         u = np.arange(0,math.pi*2,math.pi*2/resolution)
         long = np.cos(u)*self.pupil_radius_rad + self.long_rad
         lat  = np.sin(u)*self.pupil_radius_rad + self.lat_rad
 
-        return self.eye.rads_to_cartesians(np.transpose([long,lat]))
+        return np.transpose([long,lat])
 
     def get_ellipse_param(self, points_2d):
         '''
