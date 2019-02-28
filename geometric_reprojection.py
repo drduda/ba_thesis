923f5078acf88aef67335a47451f9e088e5d8c33
marko <1995marko@gmail.com>
2019-02-28 12:21:44 +0100

    Defined different stages for cone. Defined get cone by ellipse which result in one wrong value

--- geometric_reprojection.py
+++ geometric_reprojection.py
@@ -30,8 +30,11 @@ class Cone:
     The cone is described by the coefficients which its implicit equation has.
     Attribute self.a_xx is coefficient a for term x². Its general form is:
     a*x**2 + b*y**2 + c*z**2 + 2*f*y*z + 2*g*z*x + 2*h*x*y + 2*u*x + 2*v*y + 2*w*z + d = 0
+    All variable names are copied from Safaee-Rad
     '''
-    def __init__(self, a_xx, b_yy, c_zz, f_yz, g_zx, h_xy, u_x, v_y, w_z, d):
+    def __init__(self, frame, a_xx, b_yy, c_zz, f_yz, g_zx, h_xy, u_x=None, v_y=None, w_z=None, d=None):
+
+        self.frame = frame
 
         self.a_xx = a_xx
         self.b_yy = b_yy
@@ -40,14 +43,34 @@ class Cone:
         self.f_yz = f_yz
         self.g_zx = g_zx
         self.h_xy = h_xy
+        if frame=='image':
+            if not all([u_x, v_y, w_z, d]):
+                raise TypeError('For the image frame all coefficients are needed')
+            self.u_x = u_x
+            self.v_y = v_y
+            self.w_z = w_z
 
-        self.u_x = u_x
-        self.v_y = v_y
-        self.w_z = w_z
-
-        self.d = d
+            self.d = d
 
 
     @staticmethod
-    def construct_by_ellipse():
+    def construct_by_ellipse(a_xx, h_xy, b_yy, g_x, f_y, d, gamma=1):
+        a = gamma**2 * a_xx
+        b = gamma**2 * b_yy
+        c = d
+        d = gamma**2 * d
+        f = -gamma*(f_y)
+        g = -gamma*(g_x)
+        h = gamma**2 * h_xy
+        u = gamma**2 * g_x
+        v = gamma**2 * f_y
+        w = -gamma*(d)
+
+        return Cone('camera', a, b, c, f, g, h)
+
+    def get_equation(self):
+        '''
+        Returns analytic equation of the cone which can be copy pasted to Wolfram Alpha for visi
+        :return:
+        '''
         pass
