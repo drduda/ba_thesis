0e918cbda8d6eb5f876def11734468c59774c410
marko <1995marko@gmail.com>
2019-03-06 12:01:21 +0100

    clean master version

--- ellipse_unprojection.py
+++ ellipse_unprojection.py
@@ -123,9 +123,3 @@ class Quadric:
                         (self.a_xx - self.c_zz))
         #TODO implement other cases and two vectors as output inclusive test
         return np.array([l,m,n])
-
-
-class Unprojected_Circle:
-    def __init__(self, ellipse):
-        pass
-
