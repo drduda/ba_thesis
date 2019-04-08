371f0246a5c4f2372c66badf73187c99490338f6
marko <1995marko@gmail.com>
2019-04-08 12:53:23 +0200

    New implementation of all the cones. new structure of 3d circles

--- ellipse_unprojection.py
+++ ellipse_unprojection.py
@@ -20,9 +20,9 @@ class Quadric:
 
 
 class ImpEllipse(Quadric):
-    '''
+    """
     Class for an ellipse with coefficients from its implicit function as its attributes
-    '''
+    """
 
     def __init__(self, a_xx, h_xy, b_yy, g_x, f_y, d):
         self.a_xx = a_xx
@@ -32,36 +32,31 @@ class ImpEllipse(Quadric):
         self.f_y = f_y
         self.d = d
 
+    @staticmethod
+    def construct_by_param(center_x, center_y, rotation):
+        pass
 
-class Cone:
+
+class Cone(Quadric):
     '''
-    Can represent a quadric with max. 3 dimensions which is described by the coefficients which its implicit equation has.
-    Attribute self.a_xx is coefficient a for term x². Its general form is:
+    Represents a cone which is described by the coefficients which its implicit equation has.
+    Attribute self.a_xx is coefficient a for term x². Its most general form is:
     a*x**2 + b*y**2 + c*z**2 + 2*f*y*z + 2*g*z*x + 2*h*x*y + 2*u*x + 2*v*y + 2*w*z + d = 0
+    Here the simplest form is represented
     All variable names are copied from Safaee-Rad's paper
     '''
-    def __init__(self, frame, a_xx, b_yy, c_zz, f_yz=None, g_zx=None, h_xy=None, u_x=None, v_y=None, w_z=None, d=None):
-
-        self.frame = frame
-
+    def __init__(self, a_xx, b_yy, c_zz):
         self.a_xx = a_xx
         self.b_yy = b_yy
         self.c_zz = c_zz
 
-        if frame != 'XYZ':
-            self.f_yz = f_yz
-            self.g_zx = g_zx
-            self.h_xy = h_xy
 
-            if frame != 'camera':
-                if not all([u_x, v_y, w_z, d]):
-                    raise TypeError('For the image frame all coefficients are needed')
-                self.u_x = u_x
-                self.v_y = v_y
-                self.w_z = w_z
-
-                self.d = d
-
+class ConeCamera(Cone):
+    def __init__(self, a_xx, b_yy, c_zz, f_yz, g_zx, h_xy):
+        Cone.__init__(self, a_xx, b_yy, c_zz)
+        self.f_yz = f_yz
+        self.g_zx = g_zx
+        self.h_xy = h_xy
 
     @staticmethod
     def ellipse_2_cone(a_xx, h_xy, b_yy, g_x, f_y, d, gamma=1):
@@ -86,9 +81,7 @@ class Cone:
         u = gamma**2 * g_x
         v = gamma**2 * f_y
         w = -gamma*(d)
-
-        return Cone('camera', a, b, c, f, g, h)
-
+        return ConeCamera(a, b, c, f, g, h)
 
 
     def rotate_2_XYZ(self):
@@ -106,6 +99,16 @@ class Cone:
         #TODO RIGHT HAND RULE FULLFILLED FOR EIGVEC
         return eigvalue, eigvector
 
+
+class ConeXYZ(Cone):
+    def __init__(self, a_xx, b_yy, c_zz):
+        if not (a_xx>0 and b_yy>0 and c_zz<0):
+            raise ValueError("Coefficients do not form a cone")
+        else:
+            Cone.__init__(self, a_xx, b_yy, c_zz)
+
+
+class lmn:
     def get_surface_normal(self):
         if not self.frame == "XYZ":
             raise TypeError('Only XYZ frame as input allowed')
@@ -121,16 +124,22 @@ class Cone:
         return np.array([l,m,n])
 
 
-class Unprojected_Circles():
+class ThreeDimensionalCircle:
+    def __init__(self, position, orientation):
+        self.position = position
+        self.orientation = orientation
+
+
+class Double3DCircle:
     '''
     More pseudo than real code
     '''
-    def __init__(self, orientation, position):
-        self.orientation = orientation
-        self.position = position
+    def __init__(self, pos3DCircle, neg3DCircle):
+        self.pos3DCircle = pos3DCircle
+        self.neg3DCircle = neg3DCircle
 
     @staticmethod
-    def construct(ellipse):
+    def constructByParamEllipse(ellipse):
         # TODO ELLIPSE AS INPUT
         cone_camera = Cone.ellipse_2_cone()
         XYZ, rot_2_camera_matrix = cone_camera.rotate_2_XYZ()
@@ -138,8 +147,3 @@ class Unprojected_Circles():
         # Getting the plane normal first and then transform it to camera frame
         orientation = cone_XYZ.get_surface_normal()  # times rot matrix
 
-    def get_first_circle(self):
-        pass
-
-    def get_second_circle(self):
-        pass_
