4f000484a4277d198b0c7baea266f866406ea2f2
marko <1995marko@gmail.com>
2019-04-29 14:42:28 +0200

    Created new projection test

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -8,9 +8,13 @@ class TestProjeciton(TestCase):
         from projection import Eye, Pupil
         import numpy as np
         e = Eye(np.array([0.0, -5.0, 0.0]), 1.2)
-        p = Pupil(e, [0.0, 0.0, 0,2], resolution=50).get_ellipse_param_dict()
 
+        p = Pupil(e, [0.0, 0.0, 0.2], resolution=50).get_ellipse_param_dict()
         self.assertAlmostEqual(0, p["x_center"])
         self.assertAlmostEqual(0, p["y_center"])
-        self.assertAlmostEqual(p["maj"], p["min"])
-        self.assertAlmostEqual(0, p["rot"])
+        self.assertAlmostEqual(p["maj"], p["min"], places)
+
+        p = Pupil(e, [30.0, 30.0, 0.2], resolution=50).get_ellipse_param_dict()
+        self.assertGreater(p["x_center"], 0)
+        self.assertGreater(p["y_center"], 0)
+        self.assertAlmostEqual(p["rot"], 135, delta=15)
